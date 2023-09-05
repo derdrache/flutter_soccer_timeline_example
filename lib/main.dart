75bcfa6588572885cf71ffe6ecfb67a329d47708
dominik <dominik.mast.11@gmail.com>
2023-09-05 05:01:35 +0200

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'game_data.dart';
 
 void main() {
   runApp(const MyApp());
@@ -29,97 +30,314 @@ class MyApp extends StatelessWidget {
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        textTheme: const TextTheme(bodyMedium: TextStyle(color: Colors.white)),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+        body: Container(
+      decoration: const BoxDecoration(
+          gradient: LinearGradient(
+              colors: [Color(0xff127a67), Color(0xff363d48)],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight)),
+      child: SafeArea(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            AppBar(
+              title: Center(
+                child: Text(
+                  gameData["gameDate"],
+                  style: const TextStyle(color: Color(0xff363d48)),
+                ),
+              ),
+              backgroundColor: Colors.transparent,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            const ScoreBoard(),
+            const Expanded(child: TimeLine())
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    ));
+  }
+}
+
+class ScoreBoard extends StatelessWidget {
+  const ScoreBoard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 140,
+      margin: const EdgeInsets.only(left: 15, right: 15),
+      decoration: BoxDecoration(
+          color: Colors.white.withOpacity(0.2),
+          borderRadius: BorderRadius.circular(10)),
+      child: Row(
+        children: [
+          Expanded(
+              child: Padding(
+                  padding: const EdgeInsets.only(top: 20, bottom: 40),
+                  child: Image.asset(gameData["team1"]["image"]))),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                height: 60,
+                width: 50,
+                decoration: BoxDecoration(
+                    color: const Color(0xff2c524c),
+                    borderRadius: BorderRadius.circular(10)),
+                child: Center(
+                  child: Text(
+                    gameData["team1"]["endScore"].toString(),
+                    style: const TextStyle(
+                        fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+              Container(
+                  margin: const EdgeInsets.all(10),
+                  child: Text(
+                    gameData["team1"]["name"],
+                    style: const TextStyle(
+                        fontSize: 24, fontWeight: FontWeight.bold),
+                  ))
+            ],
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                height: 60,
+                width: 50,
+                decoration: BoxDecoration(
+                    color: const Color(0xff2c524c),
+                    borderRadius: BorderRadius.circular(10)),
+                child: Center(
+                  child: Text(
+                    gameData["team2"]["endScore"].toString(),
+                    style: const TextStyle(
+                        fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+              Container(
+                  margin: const EdgeInsets.all(10),
+                  child: Text(
+                    gameData["team2"]["name"],
+                    style: const TextStyle(
+                        fontSize: 24, fontWeight: FontWeight.bold),
+                  ))
+            ],
+          ),
+          Expanded(
+              child: Padding(
+                  padding: const EdgeInsets.only(top: 20, bottom: 40),
+                  child: Image.asset(gameData["team2"]["image"]))),
+        ],
+      ),
+    );
+  }
+}
+
+class TimeLine extends StatelessWidget {
+  const TimeLine({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: gameData["gameHistory"].length,
+        shrinkWrap: true,
+        itemBuilder: (context, index) {
+          List gameHistory = gameData["gameHistory"];
+          int playTime = gameHistory[index]["time"];
+          int playerNumber = gameHistory[index]["team"];
+          String title = gameHistory[index]["title"];
+          String body = gameHistory[index]["body"];
+          double bodyLength = body.length.toDouble();
+          double sideBoxHeight = bodyLength ~/ 30 * 30;
+          bool isHalfTime = title.contains("Halftime");
+          bool isEnd = title.contains("End");
+
+          return Column(
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                      child: playerNumber == 1
+                          ? InfoBox(
+                              title: title,
+                              body: body,
+                              teamNumber: playerNumber,
+                            )
+                          : const SizedBox.shrink()),
+                  Column(
+                    children: [
+                      Container(
+                        height: sideBoxHeight,
+                        width: 2,
+                        color: Colors.black,
+                      ),
+                      if (isHalfTime)
+                        const HalfEndDisplayBox(
+                          text: "End of the first half",
+                        ),
+                      if (isEnd)
+                        const HalfEndDisplayBox(
+                          text: "End of the game. Arsenal won the game.",
+                        ),
+                      if (!isHalfTime && !isEnd)
+                        PlayTimeDisplay(playTime: playTime),
+                      Container(
+                        height: sideBoxHeight,
+                        width: 2,
+                        color: Colors.black,
+                      ),
+                    ],
+                  ),
+                  Expanded(
+                      child: playerNumber == 2
+                          ? InfoBox(
+                              title: title,
+                              body: body,
+                              teamNumber: playerNumber,
+                            )
+                          : const SizedBox.shrink()),
+                ],
+              ),
+              if (index == gameHistory.length - 1)
+                const SizedBox(
+                  height: 20,
+                )
+            ],
+          );
+        });
+  }
+}
+
+class InfoBox extends StatelessWidget {
+  final String title;
+  final String body;
+  final int teamNumber;
+
+  const InfoBox(
+      {super.key,
+      required this.title,
+      required this.body,
+      required this.teamNumber});
+
+  @override
+  Widget build(BuildContext context) {
+    double iconSize = 18;
+
+    selectIcon() {
+      bool isFoul = title.contains("Foul");
+      bool isYellow = title.contains("Yellow");
+      bool isRed = title.contains("Red");
+      bool isGoal = title.contains("Gooo");
+      bool isHurt = title.contains("Ouch");
+      bool isOffside = title.contains("Offside");
+
+      if (isFoul) return Image.asset("assets/icons/pfeifen.png");
+      if (isYellow) return Image.asset("assets/icons/yellow-card.png");
+      if (isRed) return Image.asset("assets/icons/red-card.png");
+      if (isGoal) return Image.asset("assets/icons/soccer.png");
+      if (isHurt) return Image.asset("assets/icons/first-aid-box.png");
+      if (isOffside) return Image.asset("assets/icons/offside-flag.png");
+    }
+
+    return Container(
+      margin: const EdgeInsets.only(left: 10, right: 10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          if (teamNumber == 1)
+            Row(
+              children: [
+                const Expanded(child: SizedBox.shrink()),
+                Text(
+                  title,
+                  style: const TextStyle(fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(
+                  width: 5,
+                ),
+                SizedBox(
+                    height: iconSize, width: iconSize, child: selectIcon()),
+              ],
+            ),
+          if (teamNumber == 2)
+            Row(
+              children: [
+                SizedBox(
+                  width: iconSize,
+                  height: iconSize,
+                  child: selectIcon(),
+                ),
+                const SizedBox(
+                  width: 5,
+                ),
+                Text(
+                  title,
+                  style: const TextStyle(fontWeight: FontWeight.bold),
+                )
+              ],
+            ),
+          Text(
+            body,
+            textAlign: teamNumber == 1 ? TextAlign.end : null,
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class HalfEndDisplayBox extends StatelessWidget {
+  final String text;
+
+  const HalfEndDisplayBox({super.key, required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(8),
+      decoration: BoxDecoration(
+          color: Colors.white.withOpacity(0.2),
+          borderRadius: BorderRadius.circular(10)),
+      child: Text(text),
+    );
+  }
+}
+
+class PlayTimeDisplay extends StatelessWidget {
+  final int playTime;
+
+  const PlayTimeDisplay({super.key, required this.playTime});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 30,
+      height: 30,
+      decoration: BoxDecoration(
+          border: Border.all(), borderRadius: BorderRadius.circular(20)),
+      child: Center(
+        child: Text(
+          "$playTime",
+          style: const TextStyle(fontWeight: FontWeight.bold),
+        ),
+      ),
     );
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:football_timeline_flutter/main.dart';
+import 'package:soccer_timeline_flutter/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
